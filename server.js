a97f58cacfb0fac36a09676143c6107d34abd2d3
nihil8923 <chaudharynikhil855@gmail.com>
2026-04-03 13:36:22 +0530

    fixed server initialization error

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ import cors from "cors";
 import authRoutes from "./routes/auth.js";
 import leadRoutes from "./routes/lead.js";
 
-
-app.use("/api", leadRoutes);
-
-const app = express();
+const app = express();   // MUST COME BEFORE app.use
 
 app.use(cors());
 app.use(express.json());
 
-// MongoDB Atlas connection
-mongoose.connect(process.env.MONGO_URL)
+// Routes
+app.use("/api/auth", authRoutes);
+app.use("/api", leadRoutes);
+
+// MongoDB connect
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+.catch(err => console.log(err));
 
 // Test route
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
-// Start server
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server running on port " + process.env.PORT);
-});
+// Server start
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
