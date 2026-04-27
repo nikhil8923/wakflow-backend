b4a38bfa75ba218df24ba5fea12e46fff8ec406e
Nikhil Chaudhary <chaudharynikhil855@gmail.com>
2026-04-27 12:24:06 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,6 +4,8 @@ const userSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+  resetOtp: String,
+  resetOtpExpiry: Date,
   phone: String,
 
   referralCode: String,
