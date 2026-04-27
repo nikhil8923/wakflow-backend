42fabc6d9e8f0975f704a330b05fe80eb390bdeb
Nikhil Chaudhary <chaudharynikhil855@gmail.com>
2026-04-27 13:26:33 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,20 +1,32 @@
 import nodemailer from "nodemailer";
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "wakflowautomations@gmail.com",
-    pass: "xwfhdoldqmjhffeq"
-  }
-});
-
 export const sendEmail = async (to, subject, text) => {
-    console.log("EMAIL FUNCTION STARTED");
-  console.log("TO:", to);
-  await transporter.sendMail({
-    from: "Wakflow <wakflowautomations@gmail.com>",
-    to,
-    subject,
-    text
-  });
+  console.log("🚀 EMAIL FUNCTION STARTED");
+
+  try {
+    console.log("Using EMAIL_USER:", process.env.EMAIL_USER);
+    console.log("Using EMAIL_PASS:", process.env.EMAIL_PASS ? "Loaded" : "Missing");
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    });
+
+    const info = await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      text
+    });
+
+    console.log("✅ EMAIL SENT:", info.response);
+
+  } catch (error) {
+    console.log("❌ EMAIL ERROR:", error);
+  }
 };
